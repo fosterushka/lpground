738136159d78b379755865a9eb061e213a920ff2
idevmans <man1080@yandex.ru>
2021-01-01 19:42:52 +0400

    add keys for ssl in git ignore, pwa wip

--- expressjs/README.md
+++ expressjs/README.md
@@ -1,5 +1,5 @@
 npm i <br>
-DEBUG=myapp:* npm start || npm start<br>
+DEBUG=expressjs:* npm start || npm start<br>
 https://www.npmjs.com/package/pug<br>
 https://github.com/pugjs/pug/issues/2305<br>
 https://pugjs.org/language/attributes.html<br>
--- expressjs/public/javascripts/service-worker.js
+++ expressjs/public/javascripts/service-worker.js
@@ -1,2 +1,8 @@
 //todo
 console.log('Worker connected ');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === 'image',
+    new workbox.strategies.CacheFirst()
+);
