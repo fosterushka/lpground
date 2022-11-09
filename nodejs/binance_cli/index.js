8f083a6f0e09ccd9d6dedbcad1e1cfba93e3a107
idevmans <man1080@yandex.ru>
2022-11-09 22:54:38 +0300

    parse done, feat readability

--- nodejs/binance_cli/index.js
+++ nodejs/binance_cli/index.js
@@ -3,14 +3,19 @@ import binance_api from 'node-binance-api'
 import * as dotenv from 'dotenv'
 
 const binance = new binance_api().options({
-    APIKEY: dotenv.config().BINANCE_API_KEY,
-    APISECRET: dotenv.config().BINANCE_SECRET_KEY,
+    APIKEY: dotenv.config().parsed.BINANCE_API_KEY,
+    APISECRET: dotenv.config().parsed.BINANCE_SECRET_KEY,
     useServerTime: true,
     adjustForTimeDifference: true
 });
 
+await binance.useServerTime();
 binance.balance((error, balances) => {
     if ( error ) return console.error(error);
-    console.info("balances()", balances);
-    console.info("ETH balance: ", balances.ETH.available);
-  });
+    Object.keys(balances).forEach(key => {
+        const value = balances[key]
+        if (parseFloat(value.available)) {
+            console.log(key, '=>', value.available)
+        }
+    })
+});
